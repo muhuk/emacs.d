90fafb3dc5b9a39336fa9e21e7400ce271f38803
Atamert Ölçgen <muhuk@muhuk.com>
2016-11-20 17:16:35 +0300

    Install markdown-mode

--- init.el
+++ init.el
@@ -207,6 +207,15 @@
   (setq ledger-post-use-completion-engine :ido))
 
 
+(use-package markdown-mode
+  :ensure t
+  :commands (markdown-mode gfm-mode)
+  :mode (("README\\.md\\'" . gfm-mode)
+         ("\\.md\\'" . markdown-mode)
+         ("\\.markdown\\'" . markdown-mode))
+  :init (setq markdown-command "multimarkdown"))
+
+
 (use-package org
   :ensure t
   :bind
