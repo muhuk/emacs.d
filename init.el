15e4b52db5051873e0e3f5cb3b97c6a01c19fd9e
Atamert Ölçgen <muhuk@muhuk.com>
2019-03-13 18:31:21 +0800

    Use pandoc to render Markdown

--- init.el
+++ init.el
@@ -286,7 +286,7 @@
   :mode (("README\\.md\\'" . gfm-mode)
          ("\\.md\\'" . markdown-mode)
          ("\\.markdown\\'" . markdown-mode))
-  :init (setq markdown-command "multimarkdown"))
+  :init (setq markdown-command "pandoc"))
 
 
 (use-package org
