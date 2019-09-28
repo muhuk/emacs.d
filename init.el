2b156a089aad9ec61e0e1c77de46979f28ffa6ea
Atamert Ölçgen <muhuk@muhuk.com>
2019-09-28 14:41:40 +0800

    Enable company when racer is active

--- init.el
+++ init.el
@@ -536,6 +536,7 @@
   :init
   (add-hook 'rust-mode-hook #'racer-mode)
   (add-hook 'racer-mode-hook #'eldoc-mode)
+  (add-hook 'racer-mode-hook #'company-mode)
   :config
   (define-key rust-mode-map (kbd "TAB") #'company-indent-or-complete-common))
 
