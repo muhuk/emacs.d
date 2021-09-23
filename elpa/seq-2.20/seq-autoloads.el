972a4506cf73e630b0e4ee5a3887c36a4bac950b
Atamert Ölçgen <muhuk@muhuk.com>
2021-09-23 23:01:41 +0300

    Upgrade packages.

--- elpa/seq-2.20/seq-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; seq-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("seq-24.el" "seq-25.el" "seq-pkg.el" "seq.el")
-;;;;;;  (23679 43305 888681 172000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; seq-autoloads.el ends here
