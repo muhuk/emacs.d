eb6b4472843fb5958d06022039d0117417744a9b
Atamert Ölçgen <muhuk@muhuk.com>
2025-11-21 12:28:45 +0300

    Upgrade cider, clojure-mode*, clj-refactor & reformatter.

---
+++ elpa/clojure-mode-extra-font-locking-5.20.0/clojure-mode-extra-font-locking-pkg.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "clojure-mode-extra-font-locking" "5.20.0"
+  "Extra font-locking for Clojure mode."
+  '((clojure-mode "3.0"))
+  :url "https://github.com/clojure-emacs/clojure-mode"
+  :commit "d336db623e7ae8cffff50aaaea3f1b05cc4ccecb"
+  :revdesc "d336db623e7a"
+  :keywords '("languages" "lisp")
+  :authors '(("Bozhidar Batsov" . "bozhidar@batsov.dev"))
+  :maintainers '(("Bozhidar Batsov" . "bozhidar@batsov.dev")))
---
+++ elpa/reformatter-0.7/reformatter-pkg.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "reformatter" "0.7"
+  "Define commands which run reformatters on the current buffer."
+  '((emacs "24.3"))
+  :url "https://github.com/purcell/emacs-reformatter"
+  :commit "bfe3f1c6ece952d39921db16f601123bdd1748ab"
+  :revdesc "bfe3f1c6ece9"
+  :keywords '("convenience" "tools")
+  :authors '(("Steve Purcell" . "steve@sanityinc.com"))
+  :maintainers '(("Steve Purcell" . "steve@sanityinc.com")))
