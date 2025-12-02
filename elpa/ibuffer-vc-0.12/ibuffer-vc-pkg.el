50ea5f9896ede6fac1750e5a3d93c5d659215404
Atamert Ölçgen <muhuk@muhuk.com>
2025-12-02 10:45:31 +0300

    Upgrade ibuffer-vc.

---
+++ elpa/ibuffer-vc-0.12/ibuffer-vc-pkg.el
@@ -0,0 +1,11 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "ibuffer-vc" "0.12"
+  "Group ibuffer's list by VC project, or show VC status."
+  '((emacs "25.1")
+    (seq   "2"))
+  :url "https://github.com/purcell/ibuffer-vc"
+  :commit "1388d2ea18287c74a79d053619dbdfa9090c26a2"
+  :revdesc "1388d2ea1828"
+  :keywords '("convenience")
+  :authors '(("Steve Purcell" . "steve@sanityinc.com"))
+  :maintainers '(("Steve Purcell" . "steve@sanityinc.com")))
